187515eeae02545ebbec6e42b36332b04091ce92
aasoto <andresalfredosotosuarez@gmail.com>
2022-11-06 12:47:39 -0500

    proyecto adaptado a archivos locales

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,12 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      fontFamily: {
-        'delmon-delicate': ['DelmonDelicate'],
-      },
       screens: {
         'iPhoneSE': '375px',
         'iPhoneXR': '414px',
         'iPhone12Pro': '390px',
         'GalaxyFold': '280px',
+        'smd': '695px',
       },
     },
   },
