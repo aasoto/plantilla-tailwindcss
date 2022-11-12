affd710e836a752058573ec1824556bdedfcc44d
aasoto <andresalfredosotosuarez@gmail.com>
2022-11-12 11:04:34 -0500

    PÁGINA WEB ADAPTADA A ARCHIVOS LOCALES. 1. Se instaló ckeditor através de Laravel Mix. 2. Se configuraron todos los archivos para que trabajen solo con    paquete y archivos locales. ----- PÁGINA LISTA PARA PASAR A PRODUCCIÓN -----

--- src/app.js
+++ src/app.js
@@ -3,3 +3,22 @@ import Alpine from 'alpinejs'
 window.Alpine = Alpine
  
 Alpine.start()
+
+
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic/build/ckeditor';
+
+
+var ready = (callback) => {
+  if (document.readyState != "loading") callback();
+  else document.addEventListener("DOMContentLoaded", callback);
+}
+
+ready(() => { 
+  ClassicEditor
+  .create(document.querySelector('.ckeditor'))
+    .catch(error => {
+      console.log(`error`, error)
+    });
+});
+
+
